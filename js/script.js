c7b5139cc535a1497fe71d61cd789c1d53dd5fab
Abdelmalek123-Ennani <67714218+Abdelmalek123-Ennani@users.noreply.github.com>
2020-12-01 22:07:49 +0000

    html and css project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+// $(function() {
+//     // $('body').niceScroll();
+//     $("body").niceScroll({
+//         cursorcolor:"aquamarine",
+//         cursorwidth:"12px",
+//         background:"rgba(20,20,20,0.3)",
+//         cursorborder:"1px solid aquamarine",
+//         cursorborderradius:0
+//       });  
+// })
